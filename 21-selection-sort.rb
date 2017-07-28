1116fb3bcb75f10e37533942e0d7b98655e16f73
Hayson <heyson@sina.cn>
2017-07-28 21:20:52 +0800

    21-selection-sort

--- 21-selection-sort.rb
+++ 21-selection-sort.rb
@@ -1,12 +1,18 @@
+
 # 给定一数组内含数字，请实作选择排序法进行排序。
 # https://zh.wikipedia.org/wiki/选择排序
 
-def selection_sort(arr)
-  #...
+def insertion_sort(arr)
+  (0...arr.length).each do |i|
+    min, index = arr[i], i
+    (i...arr.length).each { |j| min, index = arr[j], j if arr[j] < min }
+    arr[i], arr[index] = arr[index], arr[i]
+  end
+  arr
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
-answer = selection_sort(arr)
+answer = insertion_sort(arr)
 
 puts answer.to_s # 应该是 [1, 7, 9, 42, 46, 46, 68, 77, 86, 91]
